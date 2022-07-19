cd80fc97e05266b99d9bdc91caf3e7b71ff436fe
Aboubakar Ismael <aboubakarismael16@gmail.com>
2022-07-19 09:22:55 +0800

    update

--- golang/main.go
+++ golang/main.go
@@ -1,14 +1,43 @@
 package main
 
-import "fmt"
-var v = 10
-
-func foo() *int {
-	v = 10
-	return &v
-}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	m := foo()
-	fmt.Println(&m,*m)
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please input your name: ")
+	input, err := inputReader.ReadString('\n')
+	if err != nil {
+		fmt.Printf("An error occured: %s\n", err)
+		os.Exit(1)
+	} else {
+		name := input[:len(input)-1]
+		fmt.Printf("Hello, %s! What can I do for you?\n", name)
+	}
+
+	for {
+		input, err = inputReader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("An error occured: %s\n", err)
+			continue
+		}
+		input = input[:len(input)-1]
+		input = strings.ToLower(input)
+		switch input {
+		case "" :
+			continue
+		case "nothing", "bye":
+			fmt.Println("Bye!")
+			os.Exit(0)
+		case "coffee", "the":
+			fmt.Println("Enjoy your drink")
+			os.Exit(0)
+		default:
+			fmt.Println("Sorry, I didnt catch you")
+		}
+	}
 }
